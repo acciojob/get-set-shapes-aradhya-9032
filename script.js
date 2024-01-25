52e6c6bdad4873e3cb4b8ddbc35b3f32865844ab
Aradhya <120170628+aradhya-9032@users.noreply.github.com>
2024-01-25 12:51:37 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,7 +1,29 @@
-//complete this code
-class Rectangle {}
+class Rectangle {
+    constructor(width, height) {
+        this._width = width;
+        this._height = height;
+    }
 
-class Square extends Animal {}
+    get width() {
+        return this._width;
+    }
+
+    get height() {
+        return this._height;
+    }
+
+    getArea() {
+        return this._width * this._height;
+    }
+}
+
+class Square extends Rectangle {
+    // The constructor for Square can be omitted, as it will use the constructor of the parent class (Rectangle).
+
+    getPerimeter() {
+        return 4 * this.width;
+    }
+}
 
 // Do not change the code below this line
 window.Rectangle = Rectangle;
